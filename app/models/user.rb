6d95c4cd61b7914f23fba68a7bee34d0828dc792
Paul Traue <ptrauejr@gmail.com>
2017-10-08 16:48:39 -0500

    Fixed typo in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authjenticated?(remember_token)
+  def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
