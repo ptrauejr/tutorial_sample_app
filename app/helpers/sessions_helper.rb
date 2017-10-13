12683ec34fb181e3aa25104cd5605fba2af77428
Paul Traue <ptrauejr@gmail.com>
2017-10-13 13:51:35 -0500

    Adds two methods to sessions_helper to allow for friendly redirects

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,15 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # Redirects to stored location (or to tghe default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
