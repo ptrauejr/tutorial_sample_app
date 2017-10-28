f3a89502562b11ec1846dfc610f70cada8b4adee
Paul Traue <ptrauejr@gmail.com>
2017-10-27 22:01:56 -0500

    Adds integration test for the Users_Controller to confirm that non-admin users can't delete users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user      = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
@@ -14,5 +15,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
